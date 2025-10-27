d370cc69bdb9a442fb0d8e5c6672e68527a46a4f
JJGradiz <jjgradiz@uamv.edu.ni>
2025-10-27 09:43:58 -0600

    Primer commit

--- ExamenApp/Form1.Designer.cs
+++ ExamenApp/Form1.Designer.cs
@@ -37,17 +37,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 51);
+            this.label1.Location = new System.Drawing.Point(11, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.Size = new System.Drawing.Size(108, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre del paciente:";
             // 
             // tbNombre
             // 
-            this.tbNombre.Location = new System.Drawing.Point(197, 51);
+            this.tbNombre.Location = new System.Drawing.Point(148, 30);
+            this.tbNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbNombre.Name = "tbNombre";
-            this.tbNombre.Size = new System.Drawing.Size(258, 22);
+            this.tbNombre.Size = new System.Drawing.Size(194, 20);
             this.tbNombre.TabIndex = 1;
             // 
             // cmbUrgencia
@@ -56,29 +58,32 @@
             this.cmbUrgencia.Items.AddRange(new object[] {
             "Normal",
             "Emergencia"});
-            this.cmbUrgencia.Location = new System.Drawing.Point(197, 92);
+            this.cmbUrgencia.Location = new System.Drawing.Point(170, 76);
+            this.cmbUrgencia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbUrgencia.Name = "cmbUrgencia";
-            this.cmbUrgencia.Size = new System.Drawing.Size(193, 24);
+            this.cmbUrgencia.Size = new System.Drawing.Size(146, 21);
             this.cmbUrgencia.TabIndex = 2;
             // 
             // btn
             // 
-            this.btn.Location = new System.Drawing.Point(239, 135);
+            this.btn.Location = new System.Drawing.Point(200, 125);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn.Name = "btn";
-            this.btn.Size = new System.Drawing.Size(114, 35);
+            this.btn.Size = new System.Drawing.Size(86, 28);
             this.btn.TabIndex = 3;
             this.btn.Text = "Agregar paciente";
             this.btn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.ClientSize = new System.Drawing.Size(537, 366);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.cmbUrgencia);
             this.Controls.Add(this.tbNombre);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
